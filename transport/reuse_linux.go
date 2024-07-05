edeb5310c05946772adbbd5e16e321766e0e1d13
yangjiechina <1534796060@qq.com>
2024-07-05 21:43:25 +0800

    设置linux开启端口复用参数

--- transport/reuse_linux.go
+++ transport/reuse_linux.go
@@ -6,6 +6,7 @@ import "syscall"
 
 func SetReuseOpt(network, address string, c syscall.RawConn) error {
 	return c.Control(func(fd uintptr) {
-		syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, unix.SO_REUSEADDR, 1)
+		syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
+		syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, 0xF, 1)
 	})
 }
