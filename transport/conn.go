20772976f56cdacc5460a450723e9bb3eb0eac1d
yangjiechina <1534796060@qq.com>
2024-07-10 20:07:07 +0800

    支持异步发送

--- transport/conn.go
+++ transport/conn.go
@@ -1,33 +1,76 @@
 package transport
 
 import (
+	"context"
 	"net"
 	"time"
 )
 
 // Conn 为连接句柄扩展public data字段
 type Conn struct {
-	conn net.Conn
-	//接收缓冲区
-	buffer []byte
-	Data   interface{}
+	conn             net.Conn
+	buffer           []byte      //接收缓冲区
+	pendingSendQueue chan []byte //等待发送的数据队列
+	cancelFunc       func()
+	cancelCtx        context.Context
 
-	closeCb func(conn net.Conn, err error)
+	Data    interface{}                    //绑定参数
+	closeCb func(conn net.Conn, err error) //主动调用Close时回调
 }
 
 func (c *Conn) Read(b []byte) (n int, err error) {
 	return c.conn.Read(b)
 }
 
+func (c *Conn) doAsyncWrite() {
+	for {
+		select {
+		case <-c.cancelCtx.Done():
+			return
+		case data := <-c.pendingSendQueue:
+			c.conn.Write(data)
+			break
+		}
+	}
+}
+
+func (c *Conn) EnableAsyncWriteMode(queueSize int) {
+	c.pendingSendQueue = make(chan []byte, queueSize)
+	c.cancelCtx, c.cancelFunc = context.WithCancel(context.Background())
+	go c.doAsyncWrite()
+}
+
+type ZeroWindowSizeError struct {
+}
+
+func (z ZeroWindowSizeError) Error() string {
+	return "zero window size"
+}
+
 func (c *Conn) Write(b []byte) (n int, err error) {
-	return c.conn.Write(b)
+	if c.cancelCtx != nil {
+		select {
+		case c.pendingSendQueue <- b:
+			return len(b), nil
+		default:
+			return 0, &ZeroWindowSizeError{}
+		}
+	} else {
+		return c.conn.Write(b)
+	}
 }
 
 func (c *Conn) Close() error {
+	err := c.conn.Close()
+
 	if c.closeCb != nil {
 		c.closeCb(c, nil)
 	}
-	return c.conn.Close()
+
+	if c.cancelCtx != nil {
+		c.cancelFunc()
+	}
+	return err
 }
 
 func (c *Conn) LocalAddr() net.Addr {
@@ -52,4 +95,8 @@ func (c *Conn) SetWriteDeadline(t time.Time) error {
 
 func (c *Conn) ReallocateRecvBuffer(size int) {
 	c.buffer = make([]byte, size)
+}
+
+func NewConn(conn net.Conn) *Conn {
+	return &Conn{conn: conn}
 }
